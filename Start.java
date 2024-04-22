f76e282d23572e49e70fdd43f823aed364eac730
shannee-01 <167520908+shannee-01@users.noreply.github.com>
2024-04-22 13:04:11 +0530

    Added validation

--- Start.java
+++ Start.java
@@ -7,6 +7,10 @@ import java.sql.Statement;
 public class JDBCTest {
     // JDBC URL, username, and password
     static final String JDBC_URL = "jdbc:mysql://localhost:3306/flipkart";
+if(!validate(JDBC_URL)){
+    return
+}
+    
     static final String USERNAME = "root";
     static final String PASSWORD = "123";
 
